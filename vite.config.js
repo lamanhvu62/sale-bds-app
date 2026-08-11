c940424eaaccdfde09c574938ff6e1d4d6410d24
Lâm Anh Vũ <lamanhvu62@gmail.com>
2026-08-11 10:58:37 +0700

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,6 @@
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'SaleBDS',
-        short_name: 'SaleBDS',
-        description: 'Quản lý sale BĐS',
-        theme_color: '#059669',
-        icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/.*supabase\.co\/.*/i,
-            handler: 'NetworkFirst',
-            options: { cacheName: 'api-cache' }
-          }
-        ]
-      }
-    })
-  ]
-});
+  plugins: [react()],
+})
